1924b81359302306c4264477fdb67918927cd936
Riwa88 <like.a.box.of.chocolatee@gmail.com>
2025-12-02 21:36:44 +0900

    ボタンクリックで文字列を様々な形式で表示するプログラムを作ろうを再アップロード

--- kadai_010/summary.js
+++ kadai_010/summary.js
@@ -6,9 +6,9 @@ $(function(){
         $('#target').text('Hello!');
     })
     $('#fade-out').on('click', function() {
-        $('#target').fadeOut('Hello!');
+        $('#target').fadeOut();
     })
     $('#fade-in').on('click', function() {
-        $('#target').fadeIn('Hello!');
+        $('#target').fadeIn();
     })
 });
